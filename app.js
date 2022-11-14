28360db1022111d6c8e6bae2c91a881cf3ffac2a
nikhilguna <nikhilvg@umich.edu>
2022-11-14 17:08:01 -0500

    little better

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const squares = Array.from(document.querySelectorAll('.grid div'))
 
 const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37 , 38, 39
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24
+    // 30, 31, 32, 33, 34, 35, 36, 37 , 38, 39
 ]
 
 function draw() {
@@ -141,4 +141,4 @@ function shoot(e) {
     }
 }
 
-document.addEventListener('keyup', shoot)
+document.addEventListener('keydown', shoot)
